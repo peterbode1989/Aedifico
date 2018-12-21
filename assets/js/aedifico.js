493c15ca18add9b409e12ec969d4a9ed8820f4bd
Peter Bode <peterbode1989@gmail.com>
2018-12-21 22:03:17 +0100

    Some kind of working...

--- assets/js/aedifico.js
+++ assets/js/aedifico.js
@@ -28,7 +28,7 @@
             _.defaults = {
                 namespace: 'aedifico',
                 className: 'col-',
-                colCount: 12,
+                cellCount: 12,
             };
 
             _.options = $.extend({}, _.defaults, settings);
@@ -60,8 +60,10 @@
         // Add default row to rows
         _.config.rows.push({
             cells: [], // contains all added cells
-            filled: 0, // the amount filled
-            height: 0, // the total height of the row
+            minCells: 0, // the min-amount of cells
+            maxCells: parseInt(_.options.cellCount), // the max amount of cells
+            mheight: 0, // the min height of the row
+            height: 0, // the max height of the row
         });
 
         // Start collecting data
@@ -76,23 +78,31 @@
         // Reset collection
         _.config.collection = [];
 
+
         // Loop through the children to start collecting data
         for(var i = 0; i < _.config.children.length; i++) {
+            // REFACTOR!!!!
+
+            var elm = _.config.children[i];
 
             // Retrieve the relevant classname of element
-            var getClassName = $(_.config.children[i]).attr('class').split(' ').filter(function(e) {
+            var getClassName = $(elm).attr('class').split(' ').filter(function(e) {
                 return e.indexOf(_.options.className) > -1
             });
 
             // Put everything in a arbitrary object
             var bin = {
-                target: _.config.children[i],
-                parent: {},
-                x: parseInt(getClassName[0].substring(_.options.className.length)),
-                y: Math.round(parseInt($(_.config.children[i]).outerHeight())),
-                p: false,
-                rowIndex: 0,
-                py: 0,
+                parent: null,
+                child: false,
+                target: elm,
+                size: {
+                    w: parseInt(getClassName[0].substring(_.options.className.length)),
+                    h: Math.round(parseInt($(elm).outerHeight())),
+                },
+                offset: {
+                    x: 0,
+                    y: 0,
+                },
             };
 
             // Add object bin to collection
@@ -117,77 +127,101 @@
         // Warning! Everytime you add a cell, you need to increment this value
         var index = 0;
         while(index < elms.length) {
-            var writeCell = false;
-            var cell = {
-                x: currRow.filled,
-                y: 0,
-                rowIndex: rows.length-1,
-                py: 0,
-            };
-
-            var elm = elms.filter(function(elm) {
-                return (!elm.p && (elm.x + currRow.filled <= _.options.colCount))
+            // Find the first DOM-element
+            var elm = elms.filter(function(e) {
+                return (!e.parent
+                     && (e.size.w + currRow.minCells) <= currRow.maxCells)
             });
-            // console.log(elm);
 
-            if(elm.length == 0) {
-                writeCell = true;
-                // find open spot below cell?
-                cell = elms.filter(function(elm) {
-                    return (elm.p && elm.rowIndex == rows.length-1 && ((currRow.height - elm.y) > 0))
-                });
-
-                if(cell.length > 0) {
-                    cell = {
-                        x: cell[0].parent.x,
-                        y: cell[0].y + cell[0].parent.py,
-                        py: cell[0].y + cell[0].parent.y,
-                        rowIndex: cell[0].rowIndex,
-                    };
-
-                    var space = currRow.height - cell.y;
-                    var elm = elms.filter(function(elm) {
-                        return (!elm.p && (parseInt(elm.y) <= space))
-                    });
-                } else {
-                    console.log('this happens');
-                }
-            }
-            // console.log(elm);
-
-            // var elm = elms[index];
-
+            // Check if any cells fit in the current row
             if(elm.length > 0) {
+                // Get the first element
                 elm = elm[0];
 
-                if(elm.y > currRow.height) currRow.height = elm.y;
-                elm.p = true;
-                elm.rowIndex = cell.rowIndex;
+                // Update current element
+                elm.offset.x = currRow.minCells;
+                elm.parent = true;
 
-                console.log(cell);
-
-                elm.parent = cell;
-
+                // Insert/update the row information
+                currRow.minCells += elm.size.w;
                 currRow.cells.push(elm);
-                currRow.filled += elm.x;
+
+                // If the new element has a greater height then the row,
+                // update the current row
+                if(elm.size.h < currRow.mheight) {
+                    currRow.mheight = elm.size.h;
+                }
+                if(elm.size.h > currRow.height) {
+                    if(currRow.mheight == 0) currRow.mheight = elm.size.h;
+                    currRow.height = elm.size.h;
+                }
             } else {
-                // Add default row to rows
-                _.config.rows.push({
-                    cells: [], // contains all added cells
-                    filled: 0, // the amount filled
-                    height: 0, // the total height of the row
+                // Find the a open spot
+                var xs = elms.filter(function(e) {
+                    return (e.parent && !e.child
+                         && (currRow.mheight == (e.size.h + e.offset.y)))
                 });
 
-                currRow = rows[rows.length-1];
+                if(xs.length > 0) {
+                    xs = xs[0];
 
-                index--;
+                    var elm = elms.filter(function(e) {
+                        return (!e.parent && (e.size.w <= xs.size.w))
+                    });
+                    console.log(elm);
+
+                    if(elm.length > 0) {
+                        elm = elm[0];
+
+
+                        elm.offset.y = xs.offset.y + xs.size.h;
+                        elm.offset.x = xs.offset.x;
+                        elm.parent = true;
+                        currRow.cells.push(elm);
+
+                        if(elm.size.w < xs.size.w) {
+                            // Create/add ghost cell
+                            var temp = {
+                                offset: {
+                                    x: elm.size.w + xs.offset.x,
+                                    y: xs.offset.y + xs.size.h,
+                                },
+                                size: {
+                                    w: xs.size.w - elm.size.w,
+                                    h: elm.size.h,
+                                },
+                                parent: true,
+                                child: false,
+                                target: null,
+                            };
+                            currRow.cells.push(temp);
+                        }
+
+                        xs.child = true;
+
+                        if((elm.offset.y + elm.size.h) > currRow.mheight) {
+                            currRow.mheight = (elm.offset.y + elm.size.h);
+                        }
+
+
+                        // break;
+                    } else {
+                        console.warn('BREAK Y');
+                    }
+                } else {
+                    console.warn('BREAK X');
+                }
+
+
             }
+
+
 
             index++;
         }
 
         // console.table(elms);
-        console.table(rows);
+        // console.table(rows);
 
         _.apply();
     }
@@ -195,22 +229,28 @@
     Aedifico.prototype.apply = function() {
         var _ = this;
 
-        var offsetY = 0;
-
         for(var i = 0; i < _.config.rows.length; i++) {
             var row = _.config.rows[i];
 
-
-
             for(var j = 0; j < row.cells.length; j++) {
-                var child = row.cells[j];
-                console.log(child);
-                child.target.style.top = (offsetY + child.parent.py) + 'px';
-                child.target.style.left = (child.parent.x > 0 ? (100 / (_.options.colCount / child.parent.x)) : 0) + '%';
-                // child.target.style.height = child.y + 'px';
+                var elm = row.cells[j];
+
+                if (!elm.target) {
+
+                    // draw ghost cells
+                    var btn = document.createElement('div');
+                    btn.className = 'ghost ' + _.options.className + elm.size.w;
+                    _.config.parent.append(btn);
+                    elm.target = btn;
+
+                }
+                elm.target.style.top = elm.offset.y + 'px';
+                elm.target.style.left = 100 / (row.maxCells / elm.offset.x) + '%';
+                elm.target.style.height = elm.size.h + 'px';
+
+
 
             }
-            offsetY = row.height;
         }
     }
 
@@ -225,106 +265,6 @@
             }, 200);
         }
     }
-
-    // Aedifico.prototype.__proto__ = {
-    //
-    //
-    //     collect: function() {
-    //
-    //     },
-    //
-    //     apply: function() {
-    //         var _ = this;
-    //
-    //         _.grid = [];
-    //     	_.grid.push({
-    //             minX: 0,
-    //             maxX: _.config.parentWidth,
-    //             y: 0,
-    //         });
-    //
-    //         // IDEA: Refactor into multiple __proto__
-    //         for(var i = 0; i < _.config.collection.length; i++) {
-    //
-    //             var elm = _.config.collection[i];
-    //             console.log(elm);
-    //
-    //             console.table(_.grid);
-    //
-    //             var t = _.grid[0];
-    //             function getSpot(elm) {
-    //                 for(var spot in _.grid) {
-    //                     var g = _.grid[spot];
-    //                     if(g.minX + elm.w <= g.maxX) return _.grid[spot];
-    //                 }
-    //
-    //                 return {
-    //                     minX: 0,
-    //                     maxX: _.config.parentWidth,
-    //                     y: _.grid.reduce((min, b) => Math.max(min, b.y), _.grid[0].y),
-    //                 };
-    //             }
-    //
-    //             t = getSpot(elm);
-    //
-    //             console.log(t);
-    //
-    //             _.config.collection[i].p.style.top = t.y + 'px';
-    //             _.config.collection[i].p.style.left = t.minX + 'px';
-    //
-    //
-    //
-    //
-    //             // var y = t.y;
-    //             // var w = (_.config.parentWidth / _.options.colCount);
-    //             // var l = (t.x * 100);
-    //             //
-    //             // _.config.collection[i].p.style.top = y + 'px';
-    //             // _.config.collection[i].p.style.left = l + '%';
-    //             // _.config.collection[i].p.style.width = _.config.collection[i].w + 'px';
-    //             // _.config.collection[i].p.style.height = _.config.collection[i].h + 'px'; //// FIXME: this spacing hotfix (-4 or +4 pixels shift from aspect-ratio calc)
-    //             _.grid.push({
-    //                 minX: Math.round(t.minX),
-    //                 maxX: Math.round(t.minX + elm.w),
-    //                 y: Math.round(t.y + elm.h),
-    //             });
-    //             t.minX += Math.round(elm.w);
-    //
-    //             // console.table(_.grid);
-    //             // console.log(t);
-    //             if(t.minX >= t.maxX) {
-    //                 let currIndex = _.grid.map((item) => item).indexOf(t);
-    //                 console.log(currIndex);
-    //                 _.grid.splice(currIndex, 1);
-    //             }
-    //             console.log('\n\n\n\n');
-    //
-    //
-    //
-    //             //
-    //             // _.grid.push({
-    //             //     x: index / _.options.colCount,
-    //             //     y: Math.round(_.config.collection[i].h + y),
-    //             // });
-    //
-    //         }
-    //     },
-    //
-    //     find: function(elm) {
-    //         var _ = this;
-    //
-    //         return _.grid.reduce(function(prev, curr) {
-    //             console.table({prev,curr, elm});
-    //
-    //             // if(prev.minX + elm.w <= prev.)
-    //             // if (prev.y < curr.y || prev.x < curr.x) return prev;
-    //             // if (prev.y > curr.y || prev.x > curr.x) return curr;
-    //             // return 0;
-    //         });
-    //     }
-    //
-    //
-    // }
 
     $.fn.aedifico = function() {
         var _ = this;
